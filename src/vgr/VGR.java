7b58c0b16472f589b222b088a70a0518e56bc2e5
Adam Abenius <adam.abenius@d05.se>
2012-05-25 11:37:07 +0200

    Test

---
+++ src/vgr/VGR.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vgr;
+
+/**
+ *
+ * @author AdamAbenius
+ */
+public class VGR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
